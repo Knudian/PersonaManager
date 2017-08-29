5e8412f4e734593d8e72ac6335b1065906f30934
Kévin Mèche <kevin.meche.pro@gmail.com>
2017-08-29 15:40:28 +0200

    Starting HumanFactory

--- src/main/java/PersonaManager/Factory/HumanFactory.java
+++ src/main/java/PersonaManager/Factory/HumanFactory.java
@@ -1,4 +1,14 @@
 package PersonaManager.Factory;
 
+import PersonaManager.Model.Human;
+
 public class HumanFactory {
+
+    public static Human fromJson(String inputDatas){
+        Human human = new Human();
+
+        return human;
+    }
+
+    public static String toJson(Human human, )
 }
